6f91adf3d610af4ddcbafdbe279b40a6d937c806
interale14 <aleinteriano1Qgmail.com>
2020-10-08 15:56:11 -0600

    first commit

---
+++ src/screens/MovieListScreen.js
@@ -0,0 +1,12 @@
+//Importar los modulos necesarios
+import React from "react";
+import {StyleSheet, Text} from "react-native";
+
+//Variable que contiene la pantalla
+const MovieListScreen = () => {
+    return <Text>Esta es la pantalla de listado</Text>;
+}
+
+const styles = StyleSheet.create({});
+
+export default MovieListScreen;
